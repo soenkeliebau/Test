a57d962dc27923379297f7eb238631cfee4016a4
Sönke <soenke.liebau@gmail.com>
2013-01-16 23:20:06 +0100

    Initial commit.

---
+++ src/test.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nobbert
+ * Date: 16.01.13
+ * Time: 23:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
